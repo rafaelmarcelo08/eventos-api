9e91b593558e507ff8d8bf996c179f292b639de9
Rafael <rafael.2018.ads@gmail.com>
2021-03-12 22:45:31 -0300

    Projeto iniciado

---
+++ src/main/java/br/edu/pucgoias/eventosapi/EventosApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.pucgoias.eventosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/pucgoias/eventosapi/EventosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.pucgoias.eventosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
