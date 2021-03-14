8b9aca0111c403aeeee185de17332a06dc6ec305
Rafael <rafael.2018.ads@gmail.com>
2021-03-14 19:06:35 -0300

    Criado o metodo iniciar e testado a requisição

--- src/main/java/br/edu/pucgoias/eventosapi/controller/EventoController.java
+++ src/main/java/br/edu/pucgoias/eventosapi/controller/EventoController.java
@@ -1,5 +1,14 @@
 package br.edu.pucgoias.eventosapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EventoController {
+	
+	@GetMapping("/")
+	public String iniciar() {
+		return "Aplicação iniciada.";
+	}
 
 }
