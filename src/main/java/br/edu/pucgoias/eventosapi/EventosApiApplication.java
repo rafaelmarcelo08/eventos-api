8cde282bb9ca9f7e5bd922ebb3975169c788313b
Rafael <rafael.2018.ads@gmail.com>
2021-03-14 18:47:47 -0300

    Adicionado swagger na aplicação e testado

--- src/main/java/br/edu/pucgoias/eventosapi/EventosApiApplication.java
+++ src/main/java/br/edu/pucgoias/eventosapi/EventosApiApplication.java
@@ -3,6 +3,9 @@ package br.edu.pucgoias.eventosapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class EventosApiApplication {
 
@@ -11,6 +14,3 @@ public class EventosApiApplication {
 	}
 
 }
-
-
-
