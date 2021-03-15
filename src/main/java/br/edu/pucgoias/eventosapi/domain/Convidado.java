56770c32ed90fdb4b32ba4c4cba4ef61bde03dbb
Rafael <rafael.2018.ads@gmail.com>
2021-03-15 11:08:58 -0300

    Criado os astributos das entidades

--- src/main/java/br/edu/pucgoias/eventosapi/domain/Convidado.java
+++ src/main/java/br/edu/pucgoias/eventosapi/domain/Convidado.java
@@ -1,5 +1,9 @@
 package br.edu.pucgoias.eventosapi.domain;
 
 public class Convidado {
+	
+	private Integer id;
+	private String nome;
+	private Integer idade;
 
 }
--- src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
+++ src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
@@ -1,5 +1,16 @@
 package br.edu.pucgoias.eventosapi.domain;
 
+import java.util.Date;
+
 public class Evento {
 
+	private Integer id;
+	private String nome;
+	private String local;
+	private Double valor;
+	private Date data;
+	
+	
+	
+	
 }
