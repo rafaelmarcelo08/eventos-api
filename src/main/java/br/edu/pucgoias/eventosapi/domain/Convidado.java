d66cb274e21968087c75c00e1a9779a92a2d31e7
Rafael <rafael.2018.ads@gmail.com>
2021-03-15 11:16:43 -0300

    Criado os construtores e get e set das entidades

--- src/main/java/br/edu/pucgoias/eventosapi/domain/Convidado.java
+++ src/main/java/br/edu/pucgoias/eventosapi/domain/Convidado.java
@@ -1,9 +1,42 @@
 package br.edu.pucgoias.eventosapi.domain;
 
 public class Convidado {
-	
+
 	private Integer id;
 	private String nome;
 	private Integer idade;
 
+	Convidado() {
+	}
+
+	public Convidado(Integer id, String nome, Integer idade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
 }
--- src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
+++ src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
@@ -9,8 +9,56 @@ public class Evento {
 	private String local;
 	private Double valor;
 	private Date data;
-	
-	
-	
-	
+
+	Evento() {
+	}
+
+	public Evento(Integer id, String nome, String local, Double valor, Date data) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.local = local;
+		this.valor = valor;
+		this.data = data;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
 }
