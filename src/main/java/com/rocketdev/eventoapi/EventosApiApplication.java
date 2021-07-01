dd76911b8fdd81fca0b5b29c88266b47d6ed1dc7
Rafael <rafael.2018.ads@gmail.com>
2021-07-01 13:55:56 -0300

    Adicionado o 'CrudRepository' na classe de persistencia de Evento

--- src/main/java/com/rocketdev/eventoapi/EventosApiApplication.java
+++ src/main/java/com/rocketdev/eventoapi/EventosApiApplication.java
@@ -2,7 +2,6 @@ package com.rocketdev.eventoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
--- src/main/java/com/rocketdev/eventoapi/repositories/EventoRepository.java
+++ src/main/java/com/rocketdev/eventoapi/repositories/EventoRepository.java
@@ -1,5 +1,14 @@
 package com.rocketdev.eventoapi.repositories;
 
-public interface EventoRepository {
+import org.springframework.data.repository.CrudRepository;
+import com.rocketdev.eventoapi.domain.Evento;
+
+/**
+ * Interface que define as operações da camada de persistencia de Evento
+ * 
+ * @author Rafael Marcelo
+ *
+ */
+public interface EventoRepository extends CrudRepository<Evento, Integer> {
 
 }
