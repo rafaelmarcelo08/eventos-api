db85ced63518a233fac73d3baccfec6330795a73
Rafael <rafael.2018.ads@gmail.com>
2021-07-01 13:30:11 -0300

    Adicionado as anotações na classe Evento

---
+++ bin/README.md
@@ -0,0 +1,24 @@
+## Projeto desenvolvido com Java - Spting Boot 🚀
+
+## 📚 Sobre o projeto
+
+## 🛠 Tecnologias
+
+> **Back-End:**
+> - Java
+> - Spring Boot
+> - Netlify
+
+[Netlify](https://www.netlify.com/) - A Netlify é uma empresa de computação em nuvem que oferece serviços de hospedagem e back-end sem servidor para aplicativos da Web e sites estáticos.
+
+## 🚀 Como executar o projeto
+
+Para baixar o repositório local: 
+
+```bash
+git clone
+```
+
+## 📝 Licença
+
+Este projeto está sobe a licença de <strong>Rafael Marcelo<strong>
--- src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
+++ src/main/java/br/edu/pucgoias/eventosapi/domain/Evento.java
@@ -1,16 +1,41 @@
 package br.edu.pucgoias.eventosapi.domain;
 
 import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+/**
+ * Classe que representa os dados na persistencia de 'Evento'
+ * 
+ * @author Rafael Marcelo
+ * 
+ */
+@Entity
+@Table(name = "evento")
 public class Evento {
 
+	/** Atributos */
+	/** Atributos */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Integer id;
+	@Column(name = "nome_evento")
 	private String nome;
+	@Column(name = "local_evento")
 	private String local;
+	@Column(name = "valor_evento")
 	private Double valor;
+	@Column(name = "data_evento")
 	private Date data;
 
-	Evento() {
+	/** Construtores */
+	public Evento() {
 	}
 
 	public Evento(Integer id, String nome, String local, Double valor, Date data) {
@@ -22,6 +47,7 @@ public class Evento {
 		this.data = data;
 	}
 
+	/** Get e Sets */
 	public Integer getId() {
 		return id;
 	}
@@ -60,5 +86,29 @@ public class Evento {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	/** ToString */
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", valor=" + valor + ", data=" + data + "]";
+	}
+
+	/** hashCode equals */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		return Objects.equals(id, other.id);
 	}
 }
