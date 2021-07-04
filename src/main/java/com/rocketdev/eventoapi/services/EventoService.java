878c3e020c13cba0556f42f715022f0e13346c45
Rafael <rafael.2018.ads@gmail.com>
2021-07-04 19:24:45 -0300

    Criado a interface de evento e implementado

--- src/main/java/com/rocketdev/eventoapi/services/EventoService.java
+++ src/main/java/com/rocketdev/eventoapi/services/EventoService.java
@@ -1,19 +1,63 @@
 package com.rocketdev.eventoapi.services;
 
 import java.util.List;
-
 import com.rocketdev.eventoapi.domain.Evento;
+import com.rocketdev.eventoapi.utils.EventoAPIException;
 
+/**
+ * Interface que define as operações da camada de negocio de Instrumento
+ * 
+ * @author Rafael Marcelo
+ *
+ */
 public interface EventoService {
-	
-	public Evento incluir(Evento evento);
 
-	public List<Evento> listar();
+	/**
+	 * Método para incluir um evento
+	 * 
+	 * @param evento
+	 * @return retorna Evento, caso nao retorna nenhum erro na validação
+	 * @throws EventoAPIException caso exista um erro, retorna uma
+	 *                            EventoAPIException
+	 */
+	public Evento incluir(Evento evento) throws EventoAPIException;
 
-	public Evento consultar(Integer id);
+	/**
+	 * Método para alterar um evento
+	 * 
+	 * @param evento
+	 * @return retorna Evento, caso nao retorna nenhum erro na validação
+	 * @throws EventoAPIException caso exista um erro, retorna uma
+	 *                            EventoAPIException
+	 */
+	public Evento alterar(Evento evento) throws EventoAPIException;
 
-	public void alterar(Evento evento);
+	/**
+	 * Método para excluir um evento
+	 * 
+	 * @param id
+	 * @throws EventoAPIException caso exista um erro, retorna uma
+	 *                            EventoAPIException
+	 */
+	public void excluir(Integer id) throws EventoAPIException;
 
-	public void excluir(Integer id);
+	/**
+	 * Método para consultar um evento
+	 * 
+	 * @param id
+	 * @return retorna Evento, se existir no banco de dados
+	 * @throws EventoAPIException caso exista um erro, retorna uma
+	 *                            EventoAPIException
+	 */
+	public Evento consultar(Integer id) throws EventoAPIException;
+
+	/**
+	 * Método para listar os eventos
+	 * 
+	 * @return uma lista de eventos
+	 * @throws EventoAPIException caso exista um erro, retorna uma
+	 *                            EventoAPIException
+	 */
+	public List<Evento> listar() throws EventoAPIException;
 
 }
--- src/main/java/com/rocketdev/eventoapi/services/EventoServiceImpl.java
+++ src/main/java/com/rocketdev/eventoapi/services/EventoServiceImpl.java
@@ -3,40 +3,41 @@ package com.rocketdev.eventoapi.services;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.rocketdev.eventoapi.domain.Evento;
+import com.rocketdev.eventoapi.utils.EventoAPIException;
 
 @Service
 @Transactional
 public class EventoServiceImpl implements EventoService {
 
 	@Override
-	public Evento incluir(Evento evento) {
+	public Evento incluir(Evento evento) throws EventoAPIException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Evento> listar() {
+	public Evento alterar(Evento evento) throws EventoAPIException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Evento consultar(Integer id) {
+	public void excluir(Integer id) throws EventoAPIException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Evento consultar(Integer id) throws EventoAPIException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void alterar(Evento evento) {
+	public List<Evento> listar() throws EventoAPIException {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
-	@Override
-	public void excluir(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
 }
