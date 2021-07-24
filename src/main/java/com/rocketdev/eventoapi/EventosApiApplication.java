28b29a00e7c664d0c0e16f45df69fb9d52d5b7b2
Rafael <rafael.2018.ads@gmail.com>
2021-07-24 11:24:32 -0300

    Adicionado o CommandLineRunner na aplicacao

--- src/main/java/com/rocketdev/eventoapi/EventosApiApplication.java
+++ src/main/java/com/rocketdev/eventoapi/EventosApiApplication.java
@@ -1,14 +1,21 @@
 package com.rocketdev.eventoapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-public class EventosApiApplication {
+public class EventosApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventosApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
