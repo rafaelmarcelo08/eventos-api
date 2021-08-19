6ca52bec4f25421e118f02195a8b1e764571dbda
Rafael <rafael.2018.ads@gmail.com>
2021-08-19 20:04:23 -0300

    Criado e testado a exclusao de convidado

---
+++ src/main/java/com/rocketdev/eventoapi/resources/ConvidadoResource.java
@@ -0,0 +1,24 @@
+package com.rocketdev.eventoapi.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketdev.eventoapi.repositories.ConvidadoRepository;
+
+@RestController
+@RequestMapping("/convidado")
+public class ConvidadoResource {
+
+	@Autowired
+	ConvidadoRepository convidadoRepository;
+
+
+	@DeleteMapping("/excluir/{id}")
+	public void excluir(@PathVariable Integer id) {
+		convidadoRepository.deleteById(id);
+	}
+
+}
